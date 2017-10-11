9efdb0800256ca6f008ce238bbc78a3857019850
David Jones <davidhjones89@gmail.com>
2017-10-10 22:12:04 -0500

    Update urban engine website url

--- src/components/DynamicSchedule.js
+++ src/components/DynamicSchedule.js
@@ -62,7 +62,7 @@ class CurrentScheduleComponent extends React.Component {
               <a href="https://coworkingnight.org/schedule">coworkingnight.org/schedule</a>
             </p>
             <p className="cwn-sponsors">
-            Sponsored by <a href="https://www.huntsvillewest.com" target="_blank">Huntsville West</a> and <a href="https://www.hackster.io" target="_blank">Hackster.io</a>. Presented by <a href="https://newleafdigital.org" target="_blank">Urban Engine</a>.            </p>
+            Sponsored by <a href="https://www.huntsvillewest.com" target="_blank">Huntsville West</a> and <a href="https://www.hackster.io" target="_blank">Hackster.io</a>. Presented by <a href="https://www.urbanengine.org" target="_blank">Urban Engine</a>.            </p>
           </div>
         </footer>
       </div>
