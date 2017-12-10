a79001f5c52025c362ec7e022d7eed83329ce6ab
David Jones <davidhjones89@gmail.com>
2017-12-10 09:36:18 -0600

    Migrate to new API endpoint

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,50 +6,21 @@ const axios = require('axios');
 const baseUrl = 'http://www.openhuntsville.com/api/v1/';
 
 function getCurrentId() {
-    return axios.get(`${baseUrl}/all_cwn_events`, {headers: {"Authorization": process.env.APIKEY}})
-    .then(posts => {
-      var now = Date.now();
-      var events = posts.data;
-      events.sort(function(a,b){
-        return new Date(a.date) - new Date(b.date);
-      });
+    return axios.get(`${baseUrl}/thisweeks_cwn_event`)
+    .then(event => {
 
-      var futureEvents = [];
-      events.forEach(function (event) {
-        var eventDate = new Date(event.date);
-        eventDate.setHours(eventDate.getHours() + 5);
-        if (eventDate > now) {
-          futureEvents.push(event);
-        }
-      })
-
-      if (futureEvents.length > 0)
-        return futureEvents[0].name.split('#')[1];
+      if (event != undefined && event.id != undefined)
+        return event.id;
       else
         return '';
     });
 }
 
 function getNextId() {
-  return axios.get(`${baseUrl}/all_cwn_events`, {headers: {"Authorization": process.env.APIKEY}})
-  .then(posts => {
-    var now = Date.now();
-    var events = posts.data;
-    events.sort(function(a,b){
-      return new Date(a.date) - new Date(b.date);
-    });
-
-    var futureEvents = [];
-    events.forEach(function (event) {
-      var eventDate = new Date(event.date);
-      eventDate.setHours(eventDate.getHours() + 5);
-      if (eventDate > now) {
-        futureEvents.push(event);
-      }
-    })
-
-    if (futureEvents.length > 1)
-      return futureEvents[1].name.split('#')[1];
+  return axios.get(`${baseUrl}/nextweeks_cwn_event`)
+  .then(event => {
+    if (event != undefined && event.id != undefined)
+      return event.id;
     else
       return '';
   });
@@ -57,7 +28,7 @@ function getNextId() {
 
 router.get('/cwnEvents/current', (req, res) => {
   getCurrentId().then(currentId => {
-    axios.get(`${baseUrl}/cwn_flyer/` + currentId, {headers: {"Authorization": process.env.APIKEY}})
+    axios.get(`${baseUrl}/cwn_flyer/` + currentId)
     .then(posts => {
       res.status(200).json(posts.data);
     })
@@ -69,7 +40,7 @@ router.get('/cwnEvents/current', (req, res) => {
 
 router.get('/cwnEvents/next', (req, res) => {
   getNextId().then(currentId => {
-    axios.get(`${baseUrl}/cwn_flyer/` + currentId, {headers: {"Authorization": process.env.APIKEY}})
+    axios.get(`${baseUrl}/cwn_flyer/` + currentId)
     .then(posts => {
       res.status(200).json(posts.data);
     })
@@ -80,7 +51,7 @@ router.get('/cwnEvents/next', (req, res) => {
 });
 
 router.get('/cwnEvents/:id', (req, res) => {
-  axios.get(`${baseUrl}/cwn_flyer/` + req.params.id, {headers: {"Authorization": process.env.APIKEY}})
+  axios.get(`${baseUrl}/cwn_flyer/` + req.params.id)
   .then(posts => {
     res.status(200).json(posts.data);
   })
