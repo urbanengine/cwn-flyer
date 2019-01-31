46640b6e4f08b6239353ad1797e74c910366cfad
HUGHES Tyler (Tyler) <tyler.hughes@hexagonsi.com>
2019-01-30 19:26:02 -0600

    Updated server endpoint to get response from the server

--- pages/city.js
+++ pages/city.js
@@ -8,14 +8,12 @@ const City = props => (
     </Layout>
 );
 
-City.getInitialProps = async function({ request }) {
-    const baseUrl = request
-        ? `${request.protocol}://${request.get("Host")}`
-        : "";
-    //const response = await fetch(baseUrl + '/api/schedule/coworkingnight');
-    //const data = await response.json();
+City.getInitialProps = async function( { request } ) {
+    const baseUrl = request ? `${request.protocol}://${request.get( "Host" )}` : "";
+    const response = await fetch( baseUrl + '/api/schedule/coworkingnight' );
+    const data = await response.json();
 
-    //console.log(`Show data fetched. Count: ${data.length}`);
+    console.log( `Show data fetched. Count: ${data.length}` );
 
     return {
         shows: {}
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const next = require("next");
+const express = require( "express" );
+const next = require( "next" );
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
+const app = next( { dev } );
 const handler = app.getRequestHandler();
-const Error = require("next/error");
+const Error = require( "next/error" );
 
 app.prepare()
     .then(() => {
         const server = express();
 
-        server.get("/:airportCode", (request, response) => {
+        server.get("/:airportCode", ( request, response ) => {
             const supportedCities = [
                 {
                     city: "Huntsville",
@@ -23,40 +23,43 @@ app.prepare()
                 }
             ];
 
-            var match = supportedCities.filter(function(city) {
-                if (city.airportCode === request.params.airportCode) {
+            var match = supportedCities.filter( function( city ) {
+                if ( city.airportCode === request.params.airportCode ) {
                     return city;
                 }
             });
 
-            if (match !== undefined && match.length !== 0) {
+            if ( match !== undefined && match.length !== 0 ) {
                 // user visited a city we support
-                app.render(request, response, "/city", {
-                    ...request.query,
-                    ...request.params
-                });
+                app.render( request, response, "/city", { ...request.query, ...request.params } );
             } else {
                 // user visited a city we don't yet support
                 response.statusCode = 404;
-                app.render(request, response, "/_error", {});
+                app.render( request, response, "/_error", { } );
             }
-        });
+        } );
 
-        server.get("/api/schedule/coworkingnight", (request, response) => {
-            console.log("ready to fetch the schedule");
-        });
+        server.get( "/api/schedule/coworkingnight", ( request, response ) => {
+            // construct the url for the endpoint that will give us the schedule
+            const endpoint = `${process.env.hostname}/api/v2/flyer/group/${groupId}`;
+            
+            const response = await fetch( endpoint );
+            
+            // return the results back to the client
+            return await response.json();
+        } );
 
-        server.get("*", (request, response) => {
-            return handler(request, response);
-        });
+        server.get( "*", ( request, response ) => {
+            return handler( request, response );
+        } );
 
-        server.listen(3000, error => {
-            if (error) throw error;
+        server.listen( 3000, error => {
+            if ( error ) throw error;
 
-            console.log("> Ready on http://localhost:3000");
-        });
-    })
-    .catch(exception => {
-        console.error(exception.stack);
-        process.exit(1);
-    });
+            console.log( "> Ready on http://localhost:3000" );
+        } );
+    } )
+    .catch( exception => {
+        console.error( exception.stack );
+        process.exit( 1 );
+    } );
