0fc75624ef2ea82564eb129c72cbdd7998cd384a
Tyler Hughes <tyler@tylerhughes.info>
2019-03-05 08:17:06 -0600

    Updated city to support showing errors if there is no CoWorking Night or if no workshops have been approved

--- components/Schedule.js
+++ components/Schedule.js
@@ -1,11 +1,41 @@
 import Event from "./Event";
 
-const Schedule = props => (
-    <div>
-        {/* {props.workshops.map(({ workshop }) => (
-            <Event />
-        ))} */}
-    </div>
-);
+const Schedule = props => {
+    if ( isEmpty( props.cwn ) ) {
+        return (
+            <div>
+                <h1>{props.message}</h1>
+            </div>
+        );
+    }
+    else if ( !isEmpty( props.cwn ) && ( props.cwn.workshops === undefined || props.cwn.workshops.length == 0 ) ) {
+        return (
+            <div>
+                { console.log( `schedule props: ${JSON.stringify( props )}` ) }
+                <h1>{props.message}</h1>
+            </div>
+        );
+    }
+    else {
+        return (
+            <div>
+                { console.log( `schedule: ${JSON.stringify( props )}` ) }
+                
+                <h1>Time to start rendering workshops</h1>
+            </div>
+        );
+    }
+}
+
+
+function isEmpty( obj ) {
+    for ( var key in obj ) {
+        if ( obj.hasOwnProperty( key ) ) {
+            return false;
+        }
+
+        return true;
+    }
+}
 
 export default Schedule;
--- pages/city.js
+++ pages/city.js
@@ -5,20 +5,23 @@ import Schedule from "../components/Schedule";
 const City = props => (
     <Layout>
         <title>CoWorking Night Flyer</title>
-        <Schedule></Schedule>
+        <Schedule message={props.message} cwn={props.cwn}></Schedule>
     </Layout>
 );
 
-City.getInitialProps = async function( request ) {
+City.getInitialProps = async function( context ) {
     // construct the url for the endpoint that will give us the schedule
-    const endpoint = `${process.env.HOSTNAME}/api/v2/flyer/group/${request.query.groupId}`;
+    const endpoint = `${process.env.HOSTNAME}/api/v2/flyer/group/${context.query.groupId}`;
     const response = await fetch( endpoint, { headers: { 'Authorization': process.env.APIKEY } } );
     const json = await response.json();
     
     // JSON is a promise so you have to convert it to a string before you print it.
-    //console.log( `response: ${JSON.stringify( json )}` );
+    //console.log( `response: ${JSON.stringify( json )}\n` );
 
-    return json;
+    return {
+        message: json.message,
+        cwn:json.cwn
+    };
 };
 
 export default City;
