6121062faa3ac9fee2d3973a98b2c0fd919c0e0a
Tyler Hughes <tyler@tylerhughes.info>
2019-04-03 20:54:57 -0500

    Cleaned up Event component

--- components/Event.js
+++ components/Event.js
@@ -1,3 +1,13 @@
-const Event = props => <div />;
+class Event extends Component {
+    constructor( props ) {
+        super( props );
+    }
+
+    render() {
+        return (
+            <span>TODO: Remove this before span tag before beginning work in this file.</span>
+        );
+    }
+}
 
 export default Event;
