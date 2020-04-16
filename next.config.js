427496e4d2f119f5c2debaa63ae05135b03745a5
Tyler Hughes <tyler@tylerhughes.info>
2020-04-15 19:37:23 -0500

    minor changes for improving pagespeed score

--- next.config.js
+++ next.config.js
@@ -2,17 +2,9 @@ const webpack = require( 'webpack' );
 const withCSS = require('@zeit/next-css');
 
 module.exports = withCSS( {
-    webpack( config, options ) {
-        /* config options go below */
+    /* config options go below */
 
-        config.plugins = config.plugins.filter(
-            ( plugin ) => ( plugin.constructor.name !== 'UglifyJsPlugin' )
-        );
-
-        config.plugins.push(
-            new webpack.optimize.UglifyJsPlugin()
-        );
-
-        return config;
+    optimization: {
+        minimize: false //Update this to true or false
     }
 } );
