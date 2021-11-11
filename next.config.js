1ac3e71017ebd3e398c6cd379dffd6c62ace412e
Matt Brooks <matt@msbrooks.com>
2021-11-10 23:09:38 -0600

    Trying to fix an environment issue with heroku

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 const withCSS = require('@zeit/next-css')
+
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config()
+}
+
 module.exports = withCSS({
   /* config options here */
+  env: {
+    CALENDARGROUP: process.env.CALENDARGROUP,
+    APIKEY: process.env.APIKEY
+  }
 })
