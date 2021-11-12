2f685773856cb8ddcbe005e04ea81ab65bdd5433
Matt Brooks <matt@msbrooks.com>
2021-11-12 02:48:22 +0000

    Converting shitty javascript Date() to moment to fix date/time issues

--- pages/city.js
+++ pages/city.js
@@ -23,6 +23,8 @@ class City extends Component {
     };
 
     static async getInitialProps ( context ) {
+        const timezone = 'America/Chicago';
+
         // construct the url for the endpoint that will give us the schedule
         const calendarUrl = `https://www.googleapis.com/calendar/v3/calendars/${process.env.CALENDARGROUP}/events?key=${process.env.APIKEY}`;
         const response = await fetch(calendarUrl);
@@ -32,10 +34,19 @@ class City extends Component {
         const message = "Nothing scheduled yet, check back soon!";
 
         // Get the next date of coworking night. Could be today :)
-        const nextEventDate = new Date();
-        nextEventDate.setDate(nextEventDate.getDate() + (((3 + 7 - nextEventDate.getDay()) % 7)));
-        const startDateOfCwn = moment(nextEventDate).tz('America/Chicago').startOf('day');
-        const endDateOfCwn = moment(nextEventDate).tz('America/Chicago').endOf('day');
+        let nextEventDate;
+        const dayINeed = 3; // for Wednesday
+        const today = moment().tz(timezone).isoWeekday();
+        // if we haven't yet passed the day of the week that I need:
+        if (today <= dayINeed) {
+          // then just give me this week's instance of that day
+          nextEventDate = moment().tz(timezone).isoWeekday(dayINeed);
+        } else {
+          // otherwise, give me *next week's* instance of that same day
+          nextEventDate = moment().tz(timezone).add(1, 'weeks').isoWeekday(dayINeed);
+        }
+        const startDateOfCwn = moment(nextEventDate).tz(timezone).startOf('day');
+        const endDateOfCwn = moment(nextEventDate).tz(timezone).endOf('day');
 
         // Convert Google Calendar events to the workshop format
         const workshops = [];
